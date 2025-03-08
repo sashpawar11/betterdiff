a6c69b067d7ae88690213d8c6efe2680250beb5f
sashpawar11 <saisaipawar@gmail.com>
2025-03-08 12:34:08 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Better Diff
+
+- A simple diff to compare your two files and view a unified output. 
+- Built using vanilla JS
---
+++ src/script.js
@@ -0,0 +1,14 @@
+const inputFileA = document.getElementById('inputFileA');
+const inputFileB = document.getElementById('inputFileB');
+
+
+inputFileA.addEventListener('change', () => {
+    
+    console.log('fileadded A');
+})
+
+
+inputFileB.addEventListener('change', () => {
+    
+    console.log('fileadded B');
+})
